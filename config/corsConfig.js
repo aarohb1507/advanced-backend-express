b0b77030a0cd9b73d8264adfc3e34e2e29264ec2
Aaroh Bhardwaj <aarohb.1507@gmail.com>
2025-11-02 02:49:39 +0530

    configure the cors and export

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -1,4 +1,4 @@
-const corsConfig = ()=>{
+const configureCors = ()=>{
   return({
     origin: (origin, callback)=>{
       const allowedOrigins = [
@@ -11,6 +11,15 @@ const corsConfig = ()=>{
         return callback(new Error('Not allowed by cors'))
       }
         
-    }
+    }, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept-Version'],
+    exposedHeaders: ['X-Total-Count', 'Content-Range'],
+    credentials: true, //enable support for cookies
+    preflightContinue: false,
+    maxAge: 600, //cache pre-flight responses; prevents OPTION request misuse (600 seconds)
+    optionsSuccessStatus: 204
   })
 }
+
+module.exports = { configureCors }
