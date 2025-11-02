1c65c4ff99b24468ebe79d7718a9081976670782
Aaroh Bhardwaj <aarohb.1507@gmail.com>
2025-11-02 22:54:27 +0530

    write the globalErrorHandler

---
+++ middleware/errorHandler.js
@@ -0,0 +1,32 @@
+
+class APIError extends Error{
+  constructor(message, statusCode){
+    super(message)
+    this.statusCode = statusCode
+    this.name = 'APIError'
+  }
+}
+const asyncHandler = (fn) = (req, res, next)=>{
+  Promise.resolve(fn(req, res, next)).catch(next)
+}
+
+//global error handler
+const globalErrorHandler = (err, req, res, next) => {
+  console.log(err.stack)
+  if(err instanceof APIError){
+    return res.status(err.statusCode).json({
+      status: "Error",
+      message: err.message
+    })
+  }else if(err.name === "validationError"){
+    return res.status(400).json({
+      status: "Error",
+      message: "validation error"
+    })
+  }else{
+    return res.json({
+      status: "Error",
+      message: "an unexpected error"
+    })
+  }
+}
