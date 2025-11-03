67b444765730d42d780deaf0d9a5ed7e989ba2c1
Aaroh Bhardwaj <aarohb.1507@gmail.com>
2025-11-04 01:18:18 +0530

    add basic rate limiter

--- server.js
+++ server.js
@@ -6,11 +6,14 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const { globalErrorHandler } = require('./middleware/errorHandler')
 const { urlVersioning } = require('./middleware/apiVersioning')
+const { createBasicRateLimiter } = require('./middleware/rateLimiting')
 // json middleware
-app.use(express.json())
+
 app.use(requestLogger)
 app.use(addTimeStamp)
 app.use(configureCors())
+app.use(createBasicRateLimiter(100, 15*60*1000))//100 requests allowed, per 15 mins
+app.use(express.json())
 app.use('api/v1', urlVersioning('v1'))
 app.listen(PORT, () => {
   console.log(`Server is now running on port ${PORT}`);
